df47d25646152a674e5f61f6c999787a9f1637cb
June <jh4402@nyu.edu>
2018-08-15 10:49:38 -0400

    Included prototype visual instructions

--- Fracture.ino
+++ Fracture.ino
@@ -1,105 +1,119 @@
-/*  Fracture (Game)
- *  
- *  Setup: For 3-6 players. Each player gets 5-6 tiles of a specific color.
- *         Game starts will all tiles connected, but completely segregated by color.
- *
- *  Goal: Get your tiles to be touching only other players tiles.
- *        Your tiles must be touching at least two other tiles to feel happily diverse.
- *
- *  Turns: Start from a player of choice and then go clockwise taking turns.
- *         A turn consists of fracturing the population into two sub populations,
- *         and then connecting them back together in any way you please.
- *  
- *  When a player has a tile fully diversified, it will blink in unison with other blinking Blinks of its color,
- *  first player to have all of their tiles blink wins. 
- *
- *  Game development by: Celia Pearce, Jeanie Choi, Isabella Carlson, Mike Lazer-Walker, Joshua Sloane
- *
- *  by Jonathan Bobrow
- *  9.28.2017
- */
-
-#define HAPPY_FLASH_DURATION 500
-
-Color displayColor;
-
-Color teamColors[] = {RED, BLUE, YELLOW, GREEN};
-
-byte teamIndex = 0;
-
-Timer happyFlashTimer;
-bool happyFlashOn;
+byte gameState;
+byte colorSetup;
+byte faceColors[6];
+Color colorList[] = {RED, ORANGE, YELLOW, GREEN, BLUE, MAGENTA};
+Timer victoryAnimTimer;
 
 void setup() {
+  // put your setup code here, to run once:
+  gameState = 0;
+  colorSetup = 0;
+  cycleColors(0);
+  setAllColors();
 }
 
 void loop() {
-  
-  // change team if triple clicked
-  if (buttonMultiClicked()) {
-    if (buttonClickCount() == 3) {
-      teamIndex++;
-      if(teamIndex>= COUNT_OF(teamColors)) {
-        teamIndex = 0;      
-      }
-    }
+  // put your main code here, to run repeatedly:
+  switch (gameState) {
+    case 0:
+      setupPhaseLoop();
+      break;
+    case 1:
+      break;
+    case 2:
+      gamePhaseLoop();
+      break;
+    case 3:
+      victoryPhaseLoop();
+      break;
   }
 
-  byte numNeighbors = 0;
-  bool noNeighborsOfSameColor = true;
-
-  // look at neighbors
-  FOREACH_FACE(f) {
-    if(!isValueReceivedOnFaceExpired(f)) {
-      
-      numNeighbors++;
-    
-      // if their color is the same as mine... not happy
-      if (getLastValueReceivedOnFace(f) == teamIndex) {
-        noNeighborsOfSameColor = false;
-      }
-    }
+  //you can reset any blink by triple clicking
+  if (buttonMultiClicked() == true && buttonClickCount() >= 3) { //so theoretically I can use this as a reset function
+    setup();
   }
-
-  bool isHappy = false;
-  
-  // if I have two neighbors or more and my neighbors are not my color i'm happy
-  if (numNeighbors >= 2 && noNeighborsOfSameColor) {
-    isHappy = true;
-  }
-
-  bool isOn = true;
-  
-  // if I'm happy
-  if (isHappy) {
-
-    if(happyFlashTimer.isExpired()) {
-      happyFlashOn = !happyFlashOn;
-      
-      happyFlashTimer.set(HAPPY_FLASH_DURATION);
-    }
-    
-    if(!happyFlashOn) {
-      isOn = false;
-    }
-  }
-
-  if(isOn) {
-    // have the color on the Blink raise and lower to feel more alive
-    byte bri = 220 + 35 * sin_d( (millis()/10) % 360); // oscillate between values 185 and 255
-    setColor(dim(teamColors[teamIndex], bri));
-  }
-  else {
-    setColor(OFF);
-  }
-
-  setValueSentOnAllFaces(teamIndex);
 }
 
+void setupPhaseLoop() { //Here we do the logic during setup phase
+  if (buttonSingleClicked()) {
+    //we transition through colors here
+    colorSetup += 1;
+    if (colorSetup > 5) {
+      colorSetup -= 6;
+    }
+    cycleColors(colorSetup);
+  } else if (buttonDoubleClicked()) {
+    //we transition to game state, keeping our colors
+    gameState = 2;
+  }
+}
 
-// Sin in degrees ( standard sin() takes radians )
+void cycleColors(byte col) {
+  FOREACH_FACE(f) {
+    faceColors[f] = col;
+  }
+  setAllColors();
+}
 
-float sin_d( uint16_t degrees ) {
+void gamePhaseLoop() {
+  if (buttonDown()) {
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) {
+        faceColors[f] = getLastValueReceivedOnFace(f);
+        setAllColors();
+        victoryCheck();
+      }
+    }
+  }
+}
 
-    return sin( ( degrees / 360.0F ) * 2.0F * PI   );
+void setAllColors() { //This is the function we use to update color and values
+  FOREACH_FACE(f) {
+    //this is where we actually set the color physically
+    setFaceColor(f, colorList[faceColors[f]]);
+    setValueSentOnFace(faceColors[f], f);
+  }
+}
+
+void victoryCheck() {
+  //so in here we check if we have 6 different colors
+  bool colorChecklist[6];
+  bool victoryBool = true;
+  //reset the checklist
+  for (int j = 0; j < 6; j++) {
+    colorChecklist[j] = false;
+  }
+
+  //remake the checklist
+  FOREACH_FACE(f) {
+    //grab the value of the data on that face, set to true at that position in the checklist
+    colorChecklist[faceColors[f]] = true;
+  }
+
+  //now run through the checklist and see if there are any falses
+  for (int i = 0; i < 6; i++) {
+    if (colorChecklist[i] == false) {
+      victoryBool = false;
+    }
+  }//if the bool is still true at the end of this, victory has been achieved!
+
+  if (victoryBool) {
+    gameState = 3;
+    victoryAnimTimer.set(100);
+  }
+}
+
+void victoryPhaseLoop() {
+  if (victoryAnimTimer.isExpired()) {
+    //in here we shuffle the colors along
+    for (int i = 0; i < 6; i++) {
+      faceColors[i] += 1;
+      if (faceColors[i] == 6) {
+        //this means we've gone past the last color and need to go to 0
+        faceColors[i] = 0;
+      }
+    }
+    setAllColors();
+    victoryAnimTimer.set(100);
+  }
 }
